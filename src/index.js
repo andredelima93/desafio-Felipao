432cbf45e96252547de7f361db21b155283576e9
andre de lima <andredelima33@gmail.com>
2024-05-19 16:47:27 -0300

    Projeto Heroi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("Hello");
+let nameHeroi = "Deadpool "
+let xp = 10000
+let classhero=" "
+console.log("Digite o nome do seu heroi:",nameHeroi)
+console.log("Digite a quantidade de Xp:",xp)
+if (xp<=1001){
+    classhero="Ferro"
+    console.log("O Heroi "+nameHeroi, "esta no nivel de: "+classhero); 
+}
+if (xp>=1.001 && xp<=2000){
+    classhero="Bronze"
+    console.log("O Heroi "+nameHeroi, "esta no nivel de: "+classhero); 
+
+    
+}
+if (xp>=2001 && xp<=5000){
+    classhero="Prata"
+    console.log("O Heroi "+nameHeroi, "esta no nivel de: "+classhero); 
+
+}
+if (xp>=5001 && xp<=7000){
+    classhero="Ouro"
+    console.log("O Heroi "+nameHeroi, "esta no nivel de: "+classhero); 
+}
+if (xp>=7001 && xp<=8000){
+    classhero="Platina"
+    console.log("O Heroi "+nameHeroi, "esta no nivel de: "+classhero); 
+}
+if (xp>=8001 && xp<=9000){
+    classhero="Ascendente"
+    console.log("O Heroi "+nameHeroi, "esta no nivel de: "+classhero); 
+}
+if (xp>=9001 && xp<=10000){
+    classhero="Imortal"
+    console.log("O Heroi "+nameHeroi, "esta no nivel de: "+classhero); 
+}
+    if (xp>=10001){
+    classhero="Radiante"
+    console.log("O Heroi "+nameHeroi, "esta no nivel de: "+classhero); 
+
+
+    }
+   
+
+
+
+
